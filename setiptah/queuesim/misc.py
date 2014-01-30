ba3280dd7bd2ee8c1911bcd93fe964d48d5acf20
Kyle Treleaven <treleaven.kyle@gmail.com>
2014-01-30 16:22:14 -0500

    dispatch is definitely used in taxi sims, so commit it

---
+++ setiptah/queuesim/misc.py
@@ -0,0 +1,34 @@
+
+# simulation interface is implicit;
+from setiptah.eventsim.signaling import Signal
+
+
+class Dispatch :
+    """ an abstract dispatcher pattern """
+    def join_sim(self) :
+        raise NotImplementedError('implement join_sim')
+        
+    class Interface :
+        def __init__(self, dispatch ) :
+            self.dispatch = dispatch
+            
+            """ signal """
+            self.output = Signal()
+            
+        # slot
+        def input(self, *args, **kwargs ) :
+            #print args, kwargs
+            self.dispatch.input( self, *args, **kwargs )
+            
+    def newInterface(self) :
+        return self.Interface( self )
+            
+    # slotoid
+    def input(self, interface, *args, **kwargs ) :
+        """
+        when input is called, interface has received input (args,kwargs)
+        interface may be used by dispatcher to customize behavior
+        """
+        raise NotImplementedError('implement "input" for dispatch')
+
+
